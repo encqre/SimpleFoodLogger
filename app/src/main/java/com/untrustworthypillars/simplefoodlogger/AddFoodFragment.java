878a98681cecd3b10a751088e3f36e7161db914c
encqre <jasinevicius4@gmail.com>
2020-09-16 01:40:45 +0300

    Created reusable filter for decimal number input fields.

--- app/src/main/java/com/untrustworthypillars/simplefoodlogger/AddFoodFragment.java
+++ app/src/main/java/com/untrustworthypillars/simplefoodlogger/AddFoodFragment.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.text.InputType;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,11 @@ import android.widget.Toast;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
+
+import com.untrustworthypillars.simplefoodlogger.reusable.DecimalDigitsInputFilter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //TODO fix autoscroll distances when in landscape mode (and probably on smaller res screens too)
 
@@ -111,6 +118,7 @@ public class AddFoodFragment extends Fragment {
 
         mCalories = (EditText) v.findViewById(R.id.fragment_add_food_calories);
         mCalories.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        mCalories.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(4,2)});
         mCalories.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
